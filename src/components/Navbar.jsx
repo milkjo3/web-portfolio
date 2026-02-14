41660dd5f10ba13220626918156f1e12ec7a2d92
Joseph <imjmilli21@gmail.com>
2026-02-13 18:14:43 -0700

    Introduce navbar to webpage

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,17 @@
-import './Navbar.css'
-import { Link } from 'react-router-dom'
+import './Navbar.css';
+import { Link } from 'react-router-dom';
 
 export default function NavBar (){
     return (
         <>
         <div className="nav-bar">
             <p>Joseph Milliken</p>
-            {/* <Link to="/About">About</Link> */}
-            <button className='nav-bar-btn'>Contact</button>
+            {/* <Link to="">About</Link> */}
+            {/* <button className='nav-bar-btn'>About</button> */}
+            <div className='nav-links'>
+                <Link to="/about">About</Link>
+                <button className='nav-bar-btn'>Contact</button>
+            </div>
         </div>
         </>
     )
