7bb2d67edbfc5dc5748ba1266f3024f8349546f5
Joseph <imjmilli21@gmail.com>
2026-02-14 21:48:06 -0700

    Update Hero section

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,14 @@
+export default function Hero(){
+    return (
+        <>
+        <div>
+            <h1>Software Engineer building secure and scalable applications.</h1>
+            <p>Hey i'm Joseph, a recent graduate from the University of Colorado Denver with a B.S. in Computer Science.</p>
+            <button>Projects</button>
+            <button>Experience</button>
+            <button>GitHub</button>
+            <button>Resume</button>
+        </div>
+        </>
+    )
+}
