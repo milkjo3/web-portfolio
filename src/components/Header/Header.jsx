2ca0ddb14d0052d006f6af5af95b381e9735bd2c
Joseph <imjmilli21@gmail.com>
2026-04-28 17:59:47 -0600

    Updated Header component for mobile devices

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,27 @@
-import "./Header.css";
 import { Link } from "react-router-dom";
+import { useState } from "react";
+import "./Header.css";
 
 export default function Header() {
+  const [menu, setMenu] = useState(false);
+
   return (
     <>
       <div className="nav-bar">
         <h1 className="nav-name">JOSEPH MILLIKEN</h1>
-        <div className="nav-links">
+
+        <button
+          className={`hamburger ${menu ? "open" : ""}`}
+          onClick={() => setMenu(!menu)}
+        >
+          <span></span>
+          <span></span>
+          <span></span>
+        </button>
+
+        {menu && <div className="overlay" onClick={() => setMenu(false)} />}
+
+        <div className={`nav-links ${menu ? "open" : ""}`}>
           <Link to="/about">ABOUT</Link>
           <Link to="/projects">PROJECTS</Link>
           <Link to="/contact">CONTACT</Link>
