b6e5aaaf818dd373806aa2c711f15fa5c99b45fe
Joseph <imjmilli21@gmail.com>
2026-04-28 10:23:36 -0600

    Add basename to router

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import { StrictMode } from 'react'
-import App from './App.jsx'
-import './index.css'
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { StrictMode } from "react";
+import App from "./App.jsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/web-portfolio/">
       <App />
     </BrowserRouter>
   </StrictMode>,
-)
+);
