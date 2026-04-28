0370933dd892b3e2229fdfbf9e1bad26a79390c9
Joseph <imjmilli21@gmail.com>
2026-04-28 10:23:52 -0600

    Footer text color update

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -5,21 +5,24 @@ const projectOne = {
   title: "breve",
   description:
     "URL shortener that turns long links into short, shareable ones with built-in analytics.",
-  image: "",
+  image:
+    "https://images.unsplash.com/photo-1561037404-61cd46aa615b?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
 };
 
 const projectTwo = {
   title: "CLARA",
   description:
     "AI-based second opinion tool for pathologists to make better, more accurate clinical decisions.",
-  image: "",
+  image:
+    "https://images.unsplash.com/photo-1518020382113-a7e8fc38eac9?q=80&w=1317&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
 };
 
 const projectThree = {
   title: "Transferr",
   description:
     "A peer-to-peer, multi-threaded file transfer system is designed to enable fast, scalable, and resilient data exchange directly between users without relying on a central server. ",
-  image: "",
+  image:
+    "https://images.unsplash.com/photo-1525253013412-55c1a69a5738?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
 };
 
 export default function Projects() {
