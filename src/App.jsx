d18ca6484fb1ee8f0a726b48102e34c41f3354a5
Joseph <imjmilli21@gmail.com>
2026-09-11 16:49:02 -0600

    Fix import path for About component in App

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Header from "./components/Header/Header";
 import Hero from "./components/Hero/Hero";
 import Projects from "./components/Projects/Projects";
 import Technologies from "./components/Technologies/Technologies";
-import About from "./pages/About";
+import About from "./components/About/About";
 
 function App() {
   return (
